99fe13a173a6e1078573dd2f8150be10823cef00
William Sanchez <williamsanchezdev@gmail.com>
2022-05-31 09:15:47 -0400

    Delete CODEWARS/8 kyu directory

--- CODEWARS/8 kyu/Hello World/script.js
+++
@@ -1,5 +0,0 @@
-// Description:
-// Make a simple function called greet that returns the most-famous "hello world!". add to read me
-function greet() {
-  return 'hello world!'
-}
--- CODEWARS/8 kyu/Phone Number/script.js
+++
@@ -1,9 +0,0 @@
-function createPhoneNumber(numbers) {
-  var format = '(xxx) xxx-xxxx'
-
-  for (var i = 0; i < numbers.length; i++) {
-    format = format.replace('x', numbers[i])
-  }
-
-  return format
-}
